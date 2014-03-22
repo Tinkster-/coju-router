ccb338a086ad9b21084614199bb7685fd445c139
davemackintosh <me@davemackintosh.co.uk>
2014-03-22 14:32:46 +0000

    Looks pretty stable tome.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
   let coju = require('coju');
   let router = require('coju-router')(coju);
 
-  router.get('/hi', function(req, res) {
-    console.log('Im a get request for "/hi"');
+  router.verbs(['get', 'put', 'post'], '/', function(req, res, data) {
+    res.end('Im a ' + req.method + ' request for "/hi"' + JSON.stringify(data));
   });
+
+  console.log(router.routes);
 
   coju.listen(1811);
 }
