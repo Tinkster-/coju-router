916668cdca1ac4aa1c047429296a70d4e461cd88
davemackintosh <me@davemackintosh.co.uk>
2014-06-11 11:00:25 +0100

    Added license and fixed recieving JSON as data

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Dave Mackintosh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
 {
 
   function coju_router(coju) {
@@ -31,7 +54,7 @@
 
       // Once the request is finished call our routes.
       req.on('end', function() {
-        this.splitAndObjectifyPostData();
+        this.splitAndObjectifyPostData(req.headers);
         this.runHttpMethodQueue(req.method.toLowerCase(), req, res, this.data);
       }.bind(this));
 
@@ -54,9 +77,18 @@
     }
   };
 
-  coju_router.prototype.splitAndObjectifyPostData = function() {
+  coju_router.prototype.splitAndObjectifyPostData = function(headers) {
     if (!this.data) {
       return null;
+    }
+
+    if (headers['content-type'].search('application/json') > -1) {
+      try {
+        return JSON.parse(this.data);
+      } catch(e) {
+        console.error('Could not parse the JSON request due to malformed data.');
+        return null;
+      }
     }
 
     let d = this.data;
